6d6abe1c57d43c23dc63ac44513d7cd4492d1f4b
Luis Alvarez <protoarkim@gmail.com>
2026-05-19 15:30:04 -0600

    cambios al activar entorno

--- proyecto_pytest/run_test.sh
+++ proyecto_pytest/run_test.sh
@@ -5,15 +5,24 @@ if [ !"-d venv" ]; then
     python3 -m venv venv
 fi
 
+if [ -f "venv/bin/activate" ]; then
+    source venv/bin/activate
+elif [ -f "venv/Scripts/activate" ]; then
+    soruce venv/Scripts/activate
+else
+    echo "Error: no se pudo activar el entorno virtual"
+    exit 1
+fi
+
 source venv/bin/activate
 
 echo "instalando dependencias"
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip --break-system-packages
+pip install -r requirements.txt --break-system-packages
 
 mkdir -p reports
 
 echo "Ejecutando pruebas"
-pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
+venv/bin/python -m pytest tests/ --junitxml=reports/test-results.xml --html=reports/test-results.html --self-contained-html
 
-echo "Puebas finalizadas"
+echo "Puebas finalizadas. Reportes en reports/"
